dde24c4bb1b28cd3d774377c7b0cb7f35639f0d0
Thomas Loy <ths.loy@googlemail.com>
2016-06-24 18:00:20 +0200

    API function calls

--- src/main/java/com/winthier/custom/CustomPlugin.java
+++ src/main/java/com/winthier/custom/CustomPlugin.java
@@ -4,6 +4,8 @@ import com.winthier.custom.item.Item;
 import com.winthier.custom.item.ItemListener;
 import com.winthier.custom.item.ItemRegistry;
 import lombok.Getter;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
@@ -26,5 +28,20 @@ public class CustomPlugin extends JavaPlugin {
 
     public Item findItem(String id) {
         return getItemRegistry().findItem(id);
+    }
+
+    public ItemStack spawnItem(String id) {
+        Item item = getItemRegistry().findItem(id);
+        if (item == null) return null;
+        return item.spawnItemStack();
+    }
+
+    public boolean giveItem(Player player, String itemId) {
+        Item item = getItemRegistry().findItem(itemId);
+        if (item == null) return false;
+        for (ItemStack drop: player.getInventory().addItem(item.spawnItemStack()).values()) {
+            player.getWorld().dropItem(player.getEyeLocation(), drop).setPickupDelay(0);
+        }
+        return true;
     }
 }
