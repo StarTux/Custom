5a120b2629b7b7fb2fd7ec8cf383a760eb4efe50
Thomas Loy <ths.loy@googlemail.com>
2017-01-25 14:44:59 +0100

    closeInventory() helper function

--- src/main/java/com/winthier/custom/inventory/CustomInventory.java
+++ src/main/java/com/winthier/custom/inventory/CustomInventory.java
@@ -1,11 +1,14 @@
 package com.winthier.custom.inventory;
 
+import com.winthier.custom.CustomPlugin;
+import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.inventory.InventoryDragEvent;
 import org.bukkit.event.inventory.InventoryInteractEvent;
 import org.bukkit.event.inventory.InventoryOpenEvent;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public interface CustomInventory {
     Inventory getInventory();
@@ -14,4 +17,13 @@ public interface CustomInventory {
     void onInventoryInteract(InventoryInteractEvent event);
     void onInventoryClick(InventoryClickEvent event);
     void onInventoryDrag(InventoryDragEvent event);
+
+    public static void closeInventory(final Player player) {
+        new BukkitRunnable() {
+            @Override public void run() {
+                if (!player.isValid()) return;
+                player.closeInventory();
+            }
+        }.runTask(CustomPlugin.getInstance());
+    }
 }
