f003aa73deb6b50fc729d4c8a568307350f031a5
StarTux <ths.loy@googlemail.com>
2017-02-17 19:04:19 +0100

    setSkullOwner()

--- src/main/java/com/winthier/custom/util/Dirty.java
+++ src/main/java/com/winthier/custom/util/Dirty.java
@@ -2,6 +2,7 @@ package com.winthier.custom.util;
 
 import com.winthier.custom.CustomConfig;
 import java.lang.reflect.Field;
+import java.util.UUID;
 import net.minecraft.server.v1_11_R1.*;
 import org.bukkit.craftbukkit.v1_11_R1.entity.*;
 import org.bukkit.craftbukkit.v1_11_R1.inventory.*;
@@ -79,6 +80,38 @@ public final class Dirty {
                 json = null;
             }
             return new CustomConfig(id, json);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static org.bukkit.inventory.ItemStack setSkullOwner(org.bukkit.inventory.ItemStack bukkitItem, String name, UUID uuid, String textureString) {
+        try {
+            CraftItemStack obcItem;
+            ItemStack nmsItem;
+            if (bukkitItem instanceof CraftItemStack) {
+                obcItem = (CraftItemStack)bukkitItem;
+                getFieldCraftItemStackHandle().setAccessible(true);
+                nmsItem = (ItemStack)fieldCraftItemStackHandle.get(obcItem);
+            } else {
+                nmsItem = CraftItemStack.asNMSCopy(bukkitItem);
+                obcItem = CraftItemStack.asCraftMirror(nmsItem);
+            }
+            if (!nmsItem.hasTag()) nmsItem.setTag(new NBTTagCompound());
+            NBTTagCompound tag = nmsItem.getTag();
+            if (!tag.hasKeyOfType("SkullOwner", 10)) tag.set("SkullOwner", new NBTTagCompound());
+            NBTTagCompound ownerTag = tag.getCompound("SkullOwner");
+            ownerTag.setString("Name", name);
+            ownerTag.setString("Id", uuid.toString());
+            if (!ownerTag.hasKeyOfType("Properties", 10)) ownerTag.set("Properties", new NBTTagCompound());
+            NBTTagCompound propTag = ownerTag.getCompound("Properties");
+            if (!propTag.hasKeyOfType("textures", 9)) propTag.set("textures", new NBTTagList());
+            NBTTagList textures = propTag.getList("textures", 9);
+            if (textures.size() < 1) textures.add(new NBTTagCompound());
+            NBTTagCompound texture = textures.get(0);
+            texture.setString("Value", textureString);
+            return obcItem;
         } catch (Exception e) {
             e.printStackTrace();
             return null;
