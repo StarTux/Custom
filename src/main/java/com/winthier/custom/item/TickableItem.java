1209347fcd92ac81140fe8833ac0eab65fc0b094
StarTux <ths.loy@googlemail.com>
2017-04-14 18:45:34 +0200

    Fix grammar

--- src/main/java/com/winthier/custom/item/TickableItem.java
+++ src/main/java/com/winthier/custom/item/TickableItem.java
@@ -2,7 +2,7 @@ package com.winthier.custom.item;
 
 /**
  * CustomItem instances implementing this interface will have their
- * onTick() method called once per tick whenever for every instance of
+ * onTick() method called once per tick for every instance of
  * this item being found in a player inventory.
  */
 public interface TickableItem {
