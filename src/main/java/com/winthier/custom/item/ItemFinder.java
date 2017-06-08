e843f1bebbb6375aa68543ca0d94d83c80b9ca1d
StarTux <ths.loy@googlemail.com>
2017-06-08 19:06:36 +0200

    Update items later

--- src/main/java/com/winthier/custom/item/ItemFinder.java
+++ src/main/java/com/winthier/custom/item/ItemFinder.java
@@ -3,12 +3,14 @@ package com.winthier.custom.item;
 import com.winthier.custom.CustomPlugin;
 import lombok.RequiredArgsConstructor;
 import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.scheduler.BukkitRunnable;
 
 @RequiredArgsConstructor
 public final class ItemFinder implements Listener {
@@ -16,8 +18,15 @@ public final class ItemFinder implements Listener {
 
     @EventHandler(priority = EventPriority.LOWEST)
     public void onPlayerJoin(PlayerJoinEvent event) {
-        updateInventory(event.getPlayer().getInventory());
-        updateInventory(event.getPlayer().getEnderChest());
+        Player player = event.getPlayer();
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                if (!player.isValid() || !player.isOnline()) return;
+                updateInventory(event.getPlayer().getInventory());
+                updateInventory(event.getPlayer().getEnderChest());
+            }
+        }.runTask(plugin);
     }
 
     void updateInventory(Inventory inventory) {
