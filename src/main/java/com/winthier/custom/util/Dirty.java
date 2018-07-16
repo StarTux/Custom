129f0af7d742dc1661ae415b201a356276d58601
StarTux <ths.loy@googlemail.com>
2018-07-16 19:26:01 +0200

    fromTag(), toTag() and tile entity setters and getters for more awesomeness

--- src/main/java/com/winthier/custom/util/Dirty.java
+++ src/main/java/com/winthier/custom/util/Dirty.java
@@ -2,14 +2,26 @@ package com.winthier.custom.util;
 
 import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 import net.minecraft.server.v1_13_R1.ItemStack;
+import net.minecraft.server.v1_13_R1.NBTBase;
+import net.minecraft.server.v1_13_R1.NBTTagByte;
+import net.minecraft.server.v1_13_R1.NBTTagByteArray;
 import net.minecraft.server.v1_13_R1.NBTTagCompound;
+import net.minecraft.server.v1_13_R1.NBTTagDouble;
+import net.minecraft.server.v1_13_R1.NBTTagFloat;
+import net.minecraft.server.v1_13_R1.NBTTagInt;
 import net.minecraft.server.v1_13_R1.NBTTagIntArray;
 import net.minecraft.server.v1_13_R1.NBTTagList;
+import net.minecraft.server.v1_13_R1.NBTTagLong;
+import net.minecraft.server.v1_13_R1.NBTTagLongArray;
+import net.minecraft.server.v1_13_R1.NBTTagShort;
 import net.minecraft.server.v1_13_R1.NBTTagString;
+import net.minecraft.server.v1_13_R1.TileEntity;
+import org.bukkit.craftbukkit.v1_13_R1.CraftWorld;
 import org.bukkit.craftbukkit.v1_13_R1.inventory.CraftItemStack;
 
 public final class Dirty {
@@ -141,45 +153,115 @@ public final class Dirty {
         return item;
     }
 
-    public static final class TagListWrapper {
-        private final NBTTagList list;
-
-        TagListWrapper(NBTTagList list) {
-            this.list = list;
+    public static Object fromTag(NBTBase value) {
+        if (value instanceof NBTTagCompound) {
+            // Recursive
+            NBTTagCompound tag = (NBTTagCompound)value;
+            Map<String, Object> result = new HashMap<>();
+            for (String key: tag.getKeys()) {
+                result.put(key, fromTag(tag.get(key)));
+                System.out.println(key + " => (" + result.get(key).getClass().getName() + ") " + result.get(key).toString());
+            }
+            return result;
+        } else if (value instanceof NBTTagList) {
+            // Recursive
+            NBTTagList tag = (NBTTagList)value;
+            List<Object> result = new ArrayList<>();
+            for (int i = 0; i < tag.size(); i += 1) {
+                result.add(fromTag(tag.get(i)));
+            }
+            return result;
+        } else if (value instanceof NBTTagString) {
+            return (String)((NBTTagString)value).b_();
+        } else if (value instanceof NBTTagInt) {
+            return (int)((NBTTagInt)value).e();
+        } else if (value instanceof NBTTagLong) {
+            return (long)((NBTTagLong)value).d();
+        } else if (value instanceof NBTTagShort) {
+            return (short)((NBTTagShort)value).f();
+        } else if (value instanceof NBTTagFloat) {
+            return (float)((NBTTagFloat)value).i();
+        } else if (value instanceof NBTTagDouble) {
+            return (double)((NBTTagDouble)value).asDouble();
+        } else if (value instanceof NBTTagByte) {
+            return (byte)((NBTTagByte)value).g();
+        } else if (value instanceof NBTTagByteArray) {
+            return (byte[])((NBTTagByteArray)value).c();
+        } else if (value instanceof NBTTagIntArray) {
+            return (int[])((NBTTagIntArray)value).d();
+        } else if (value instanceof NBTTagLongArray) {
+            return (long[])((NBTTagLongArray)value).d();
+        } else {
+            System.err.println("TagWrapper.fromTag: Unsupported value type: " + value.getClass().getName());
+            return value.toString();
         }
+    }
 
-        public String getString(int i) {
-            if (i < 0 || i >= list.size()) return null;
-            return list.getString(i);
-        }
+    public static Map<String, Object> getBlockTag(org.bukkit.block.Block bukkitBlock) {
+        CraftWorld craftWorld = (CraftWorld)bukkitBlock.getWorld();
+        TileEntity tileEntity = craftWorld.getTileEntityAt(bukkitBlock.getX(), bukkitBlock.getY(), bukkitBlock.getZ());
+        if (tileEntity == null) return null;
+        NBTTagCompound tag = new NBTTagCompound();
+        tileEntity.save(tag);
+        return (Map<String, Object>)fromTag(tag);
+    }
 
-        public void addString(String str) {
-            NBTTagString tag = new NBTTagString(str);
-            list.add(tag);
-        }
+    public static boolean setBlockTag(org.bukkit.block.Block bukkitBlock, Map<String, Object> json) {
+        CraftWorld craftWorld = (CraftWorld)bukkitBlock.getWorld();
+        TileEntity tileEntity = craftWorld.getTileEntityAt(bukkitBlock.getX(), bukkitBlock.getY(), bukkitBlock.getZ());
+        if (tileEntity == null) return false;
+        NBTTagCompound tag = (NBTTagCompound)toTag(json);
+        tileEntity.load(tag);
+        return true;
+    }
 
-        public TagWrapper getCompound(int i) {
-            if (i < 0 || i >= list.size()) return null;
-            return new TagWrapper((NBTTagCompound)list.get(i)); // FIXME: Check cast
-        }
-
-        public TagWrapper createCompound() {
-            NBTTagCompound compound = new NBTTagCompound();
-            list.add(compound);
-            return new TagWrapper(compound);
-        }
-
-        public int size() {
-            return list.size();
-        }
-
-        public void remove(int i) {
-            list.remove(i);
+    public static NBTBase toTag(Object value) {
+        if (value instanceof Map) {
+            // Recursive
+            NBTTagCompound tag = new NBTTagCompound();
+            for (Map.Entry<String, Object> e: ((Map<String, Object>)value).entrySet()) {
+                tag.set(e.getKey(), toTag(e.getValue()));
+            }
+            return tag;
+        } else if (value instanceof List) {
+            // Recursive
+            NBTTagList tag = new NBTTagList();
+            for (Object e: (List<Object>)value) {
+                tag.add(toTag(e));
+            }
+            return tag;
+        } else if (value instanceof String) {
+            return new NBTTagString((String)value);
+        } else if (value instanceof Integer) {
+            return new NBTTagInt((Integer)value);
+        } else if (value instanceof Long) {
+            return new NBTTagLong((Long)value);
+        } else if (value instanceof Short) {
+            return new NBTTagShort((Short)value);
+        } else if (value instanceof Float) {
+            return new NBTTagFloat((Float)value);
+        } else if (value instanceof Double) {
+            return new NBTTagDouble((Double)value);
+        } else if (value instanceof Boolean) {
+            return new NBTTagInt((Boolean)value ? 1: 0);
+        } else if (value instanceof byte[]) {
+            return new NBTTagByteArray((byte[])value);
+        } else if (value instanceof int[]) {
+            return new NBTTagIntArray((int[])value);
+        } else if (value instanceof long[]) {
+            return new NBTTagLongArray((long[])value);
+        } else {
+            System.err.println("TagWrapper.toTag: Unsupported value type: " + value.getClass().getName());
+            return new NBTTagString(value.toString());
         }
     }
 
     public static final class TagWrapper {
         private final NBTTagCompound tag;
+
+        TagWrapper() {
+            this.tag = new NBTTagCompound();
+        }
 
         TagWrapper(NBTTagCompound tag) {
             this.tag = tag;
@@ -314,36 +396,55 @@ public final class Dirty {
             tag.remove(key);
         }
 
+        public Map<String, Object> toMap() {
+            return (Map<String, Object>)fromTag(tag);
+        }
+
         public void applyMap(Map<String, Object> map) {
-            for (String key: map.keySet()) {
-                Object value = map.get(key);
-                if (value instanceof String) {
-                    setString(key, (String)value);
-                } else if (value instanceof Integer) {
-                    setInt(key, (Integer)value);
-                } else if (value instanceof Long) {
-                    setLong(key, (Long)value);
-                } else if (value instanceof Float) {
-                    setFloat(key, (Float)value);
-                } else if (value instanceof Double) {
-                    setDouble(key, (Double)value);
-                } else if (value instanceof Boolean) {
-                    setBoolean(key, (Boolean)value);
-                } else if (value instanceof Map) {
-                    TagWrapper newTag = createCompound(key);
-                    newTag.applyMap((Map<String, Object>)value);
-                } else if (value instanceof List) {
-                    TagListWrapper newList = createList(key);
-                    for (Object o: (List)value) {
-                        if (o instanceof Map) {
-                            TagWrapper newTag = newList.createCompound();
-                            newTag.applyMap((Map<String, Object>)o);
-                        } else if (o instanceof String) {
-                            newList.addString((String)o);
-                        }
-                    }
-                }
+            for (Map.Entry<String, Object> e: map.entrySet()) {
+                tag.set(e.getKey(), toTag(e.getValue()));
             }
+        }
+    }
+
+    public static final class TagListWrapper {
+        private final NBTTagList list;
+
+        TagListWrapper(NBTTagList list) {
+            this.list = list;
+        }
+
+        public String getString(int i) {
+            if (i < 0 || i >= list.size()) return null;
+            return list.getString(i);
+        }
+
+        public void addString(String str) {
+            NBTTagString tag = new NBTTagString(str);
+            list.add(tag);
+        }
+
+        public TagWrapper getCompound(int i) {
+            if (i < 0 || i >= list.size()) return null;
+            return new TagWrapper((NBTTagCompound)list.get(i)); // FIXME: Check cast
+        }
+
+        public TagWrapper createCompound() {
+            NBTTagCompound compound = new NBTTagCompound();
+            list.add(compound);
+            return new TagWrapper(compound);
+        }
+
+        public int size() {
+            return list.size();
+        }
+
+        public void remove(int i) {
+            list.remove(i);
+        }
+
+        public List<Object> toList() {
+            return (List<Object>)fromTag(this.list);
         }
     }
 }
